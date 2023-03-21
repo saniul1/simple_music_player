25c7b65c9c4f1acef242de2ab0e606a2de09f3fe
Saniul haque <haquesaniul@gmail.com>
2023-03-21 22:23:06 +0530

    ...

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,8 +9,4 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:simple_music_player/main.dart';
 
-void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const AppContainer());
-  });
-}
+void main() {}
